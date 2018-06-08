050c7801b3b28c09107eea2ca89c1c9810494035
kanvar22 <league@WTS-IM-17.attlocal.net>
2018-06-07 20:00:14 -0700

    Worked on extra module 5 practice

---
+++ src/data_structures/ArrayListPuzzle.java
@@ -0,0 +1,9 @@
+package data_structures;
+
+import java.util.ArrayList;
+
+public class ArrayListPuzzle {
+public static void main(String[] args) {
+	ArrayList <Food> avilableFood = new ArrayList<Food>();	
+}
+}
